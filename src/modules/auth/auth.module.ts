159fcd5e2891ab9892b5d8da21ae215baa2caad3
ODominguez777 <sodastereo-12@outlook.com>
2025-04-20 00:20:47 -0600

    Reducir a un dia el token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '7d' }, // Token válido por 7 días
+        signOptions: { expiresIn: '1d' }, // Token válido por 7 días
       }),
     }),
   ],
