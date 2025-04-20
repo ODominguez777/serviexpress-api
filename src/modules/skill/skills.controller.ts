8523cd4e6c52ce760a222f49f4eba326edd846f5
ODominguez777 <sodastereo-12@outlook.com>
2025-04-20 03:50:08 -0600

    checkpoint (voy a romper cosas)

--- src/modules/skill/skills.controller.ts
+++ src/modules/skill/skills.controller.ts
@@ -5,16 +5,24 @@ import {
   Param,
   Body,
   NotFoundException,
+  UseGuards,
 } from '@nestjs/common';
 import { SkillService } from './skills.service';
 import { CreateSkillDto } from './dto/create-skill.dto';
 import { ApiResponse } from '../users/dto/response.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { Roles } from 'src/utils/decorators/roles.decorators';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
 
 @Controller('skills')
 export class SkillController {
   constructor(private readonly skillService: SkillService) {}
 
   @Post()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   async createSkill(
     @Body() createSkillDto: CreateSkillDto,
   ): Promise<ApiResponse<any>> {
--- src/modules/skill/skills.module.ts
+++ src/modules/skill/skills.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SkillService } from './skills.service';
 import { SkillController } from './skills.controller';
 import { Skill, SkillSchema } from './schemas/skill.schema';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Skill.name, schema: SkillSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Skill.name, schema: SkillSchema }]),
+    AuthModule,
+    forwardRef(() => UsersModule),
+  ],
   controllers: [SkillController],
   providers: [SkillService],
-  exports: [SkillService, MongooseModule], 
+  exports: [SkillService, MongooseModule],
 })
 export class SkillModule {}
