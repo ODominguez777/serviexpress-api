6cb37956b77c03417861677eb80b169b559b2a02
ODominguez777 <sodastereo-12@outlook.com>
2025-04-30 03:05:47 -0600

    Delete methods related with quotation

--- src/modules/requests/requests.service.ts
+++ src/modules/requests/requests.service.ts
@@ -158,6 +158,21 @@ export class RequestsService {
     }
   }
 
+  async acceptRequest(
+    handymanId: string,
+    requestId: string,
+  ): Promise<ApiResponse<any>> {
+
+    if (!Types.ObjectId.isValid(requestId)) {
+      throw new BadRequestException('Invalid requestId ID');
+    }
+    if (!Types.ObjectId.isValid(handymanId)) {
+      throw new BadRequestException('Invalid handymanId ID');
+    }
+    return new ApiResponse(200, 'Request accepted successfully', {
+      requestId: requestId,
+    });
+  }
   async getClientRequests(clientId: string): Promise<Request[]> {
     const requests = await this.requestModel
       .find({ clientId: new Types.ObjectId(clientId) }) // Filtrar por el ID del cliente
@@ -245,172 +260,6 @@ export class RequestsService {
     }
     await request.save();
     return new ApiResponse(200, 'Request canceled successfully', request);
-  }
-  async createQuotation(
-    handymanId: string,
-    requestId: string,
-    createQuotationDto: CreateQuotationDto,
-  ): Promise<ApiResponse<any>> {
-    const { amount, description, action } = createQuotationDto;
-    const request = await this.requestModel.findById(requestId);
-
-    if (!request) {
-      throw new NotFoundException('Request not found');
-    }
-
-    if (request.handymanId.toString() !== handymanId) {
-      throw new ForbiddenException(
-        'You are not authorized to respond to this request',
-      );
-    }
-
-    if (request.status !== RequestStatus.PENDING) {
-      throw new ConflictException('This request has already been processed');
-    }
-
-    if (!request._id) {
-      throw new NotFoundException('Request ID not found');
-    }
-    const channelId = `request-${request._id.toString()}`;
-
-    if (action === 'accept') {
-      // Actualizar el estado de la solicitud a "accepted"
-      request.status = RequestStatus.ACCEPTED;
-      await request.save();
-
-      // Enviar mensaje de aceptación
-      const acceptanceMessage = `La solicitud ha sido aceptada.`;
-      await this.chat.sendMessage(channelId, handymanId, acceptanceMessage);
-
-      // Enviar detalles de la factura
-
-      const invoiceMessage = `Detalles de la factura: \n Costo: C$${amount} \n Descripción: ${description}`;
-      await this.chat.sendMessage(channelId, handymanId, invoiceMessage);
-
-      const newQuotation = new this.quotationModel({
-        requestId: request._id,
-        handymanId: new mongoose.Types.ObjectId(handymanId),
-        clientId: request.clientId,
-        amount,
-        description,
-        status: 'pending',
-        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      });
-      const savedQuotation = await newQuotation.save();
-      return new ApiResponse(200, 'Request accepted successfully', null);
-    } else if (action === 'reject') {
-      // Actualizar el estado de la solicitud a "rejected"
-      request.status = RequestStatus.REJECTED;
-
-      // Enviar mensaje de rechazo
-      const rejectionMessage = `La solicitud ha sido rechazada.`;
-      await this.chat.sendMessage(channelId, handymanId, rejectionMessage);
-      await request.save();
-
-      return new ApiResponse(200, 'Request rejected successfully', request);
-    }
-
-    throw new BadRequestException('Invalid action');
-  }
-  async acceptQuotation(
-    quotationId: Types.ObjectId,
-    clientId: Types.ObjectId,
-  ): Promise<ApiResponse<any>> {
-    if (!Types.ObjectId.isValid(quotationId)) {
-      throw new BadRequestException('Invalid quotation ID');
-    }
-    if (!Types.ObjectId.isValid(clientId)) {
-      throw new BadRequestException('Invalid clientId ID');
-    }
-
-    const quotation = await this.quotationModel.findById(quotationId);
-    if (!quotation) {
-      throw new NotFoundException('Quotation not found');
-    }
-    if (quotation.clientId.toString() !== clientId.toString()) {
-      throw new ForbiddenException(
-        'You are not authorized to accept this quotation',
-      );
-    }
-    if (quotation.status !== 'pending') {
-      throw new ConflictException('This quotation has already been processed');
-    }
-
-    const message = `**Cotización aceptada**\n\nLa cotización de: _${quotation.amount}_ ha sido aceptada por el cliente.`;
-    const channelId = `request-${quotation.requestId.toString()}`;
-    try {
-      await this.chat.sendMessage(channelId, this.adminId, message);
-    } catch (error) {
-      throw new BadRequestException(error.message);
-    }
-    quotation.status = QuotationStatus.ACCEPTED;
-    await quotation.save();
-
-    return new ApiResponse(200, 'Quotation accepted successfully', null);
-  }
-
-  async rejectQuotation(
-    quotationId: Types.ObjectId,
-    clientId: Types.ObjectId,
-  ): Promise<ApiResponse<any>> {
-    if (!Types.ObjectId.isValid(quotationId)) {
-      throw new BadRequestException('Invalid quotation ID');
-    }
-    if (!Types.ObjectId.isValid(clientId)) {
-      throw new BadRequestException('Invalid clientId ID');
-    }
-
-    const quotation = await this.quotationModel.findById(quotationId);
-    if (!quotation) {
-      throw new NotFoundException('Quotation not found');
-    }
-
-    if (quotation.clientId.toString() !== clientId.toString()) {
-      throw new ForbiddenException(
-        'You are not authorized to reject this quotation',
-      );
-    }
-    if (quotation.status !== 'pending') {
-      throw new ConflictException('This quotation has already been processed');
-    }
-
-    const message = `**Cotización rechazada**\n\nLa cotización de: _${quotation.amount}_ ha sido rechazada por el cliente.`;
-    const channelId = `request-${quotation.requestId.toString()}`;
-    try {
-      await this.chat.sendMessage(channelId, this.adminId, message);
-    } catch (error) {
-      throw new BadRequestException(error.message);
-    }
-    quotation.status = QuotationStatus.REJECTED;
-    await quotation.save();
-
-    return new ApiResponse(200, 'Quotation rejected successfully', null);
-  }
-
-  async getQuotationByRequestId(
-    requestId: Types.ObjectId,
-    clientId: Types.ObjectId,
-  ): Promise<ApiResponse<any>> {
-    if (!Types.ObjectId.isValid(requestId)) {
-      throw new BadRequestException('Invalid request ID');
-    }
-    if (!Types.ObjectId.isValid(clientId)) {
-      throw new BadRequestException('Invalid clientId ID');
-    }
-    const quotation = await this.quotationModel
-      .findOne({ requestId })
-      .select('_id amount description status clientId');
-
-    if (!quotation) {
-      throw new NotFoundException('No quotation found for this request');
-    }
-
-    if (quotation.clientId.toString() !== clientId.toString()) {
-      throw new ForbiddenException(
-        'You are not authorized to view this quotation',
-      );
-    }
-    return new ApiResponse(200, 'Quotation found', quotation);
   }
 
   private async validateActors(
