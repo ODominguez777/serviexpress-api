91ed3c99342a7b429b7f413569b6f90a0b35a1b2
ODominguez777 <sodastereo-12@outlook.com>
2025-05-06 00:06:36 -0600

    cors stuff

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization, x-jwt-token',
     credentials: true,
   });
   await app.listen(process.env.PORT || 4000);
