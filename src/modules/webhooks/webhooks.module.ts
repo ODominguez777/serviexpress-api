3cf99f90f5091c981478bb1972723d11f3f728e0
ODominguez777 <sodastereo-12@outlook.com>
2025-05-05 19:51:30 -0600

    delete imports not used

--- src/modules/webhooks/webhooks.module.ts
+++ src/modules/webhooks/webhooks.module.ts
@@ -6,11 +6,17 @@ import { PaymentService } from '../payment/payment.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Payment, PaymentSchema } from '../payment/schemas/payment.schema';
 import { PaymentModule } from '../payment/payment.module';
+import { RequestSchema } from '../requests/schemas/request.schema';
+import { ChatModule } from '../chat/chat.module';
+import { QuotationSchema } from '../quotations/schemas/quotation.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Payment.name, schema: PaymentSchema }]),
+    MongooseModule.forFeature([{ name: 'Request', schema: RequestSchema }]),
+    MongooseModule.forFeature([{ name: 'Quotation', schema: QuotationSchema }]),
     PaymentModule,
+    ChatModule,
   ],
   controllers: [PaypalWebhookController],
   providers: [PaypalWebhookService, OrdersService, PaymentService],
