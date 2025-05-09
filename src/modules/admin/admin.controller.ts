cfc3f16ec994c6c7db9ddb6eda7c92bb9a3ec816
ODominguez777 <sodastereo-12@outlook.com>
2025-05-09 00:36:50 -0600

    melalavo

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -9,20 +9,21 @@ import {
   HttpStatus,
   Post,
   Inject,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { ApiResponse } from '../users/dto/response.dto';
 import { BanUserDto } from '../users/dto/ban-user.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from 'src/guards/roles.guard';
-import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiParam, ApiTags, ApiQuery } from '@nestjs/swagger';
 import { Roles } from 'src/utils/decorators/roles.decorators';
 import { BanUserParamDto } from './dto/ban-user-param.dto';
 import { CHAT_ADAPTER } from '../chat/chat.constants';
 import { ChatAdapter } from '../chat/adapter/chat.adapter';
 @ApiTags('Admin')
 @Controller('admin')
-
 export class AdminController {
   constructor(
     private readonly adminService: AdminService,
@@ -38,14 +39,14 @@ export class AdminController {
     return this.adminService.userBanManagment(params.id);
   }
 
-  @Roles('admin') // Solo accesible para administradores
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard, RolesGuard) // Solo accesible para administradores
-  @Delete('users/all')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async deleteAllUsersExceptAdmins(): Promise<void> {
-    await this.adminService.deleteAllUsersExceptAdmins();
-  }
+  // @Roles('admin') // Solo accesible para administradores
+  // @ApiBearerAuth()
+  // @UseGuards(JwtAuthGuard, RolesGuard) // Solo accesible para administradores
+  // @Delete('users/all')
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  // async deleteAllUsersExceptAdmins(): Promise<void> {
+  //   await this.adminService.deleteAllUsersExceptAdmins();
+  // }
 
   @Post('create-admin-streamio')
   async createAdminStreamio(): Promise<ApiResponse<any>> {
@@ -56,5 +57,53 @@ export class AdminController {
       'https://lh3.googleusercontent.com/a/ACg8ocJ1nT8Gggcuxq0anYgGbmTloPUiRgYFcYwy5gJAl_AUfPVMwWY=s360-c-no',
     );
     return new ApiResponse(200, 'Admin Streamio created successfully', null);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard) // Solo accesible para administradores
+  @Roles('admin')
+  @Get('getAllUsers')
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'Número de página (opcional, por defecto 1)',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description: 'Cantidad de resultados por página (opcional, por defecto 10)',
+  })
+  async getAllUsers(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ): Promise<ApiResponse<any>> {
+    const users = await this.adminService.getAllUsers(page, limit);
+    return new ApiResponse(200, 'Users retrieved successfully', users);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard) // Solo accesible para administradores
+  @Roles('admin')
+  @Get('getAllReports')
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'Número de página (opcional, por defecto 1)',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description: 'Cantidad de resultados por página (opcional, por defecto 10)',
+  })
+  async getAllReports(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ): Promise<ApiResponse<any>> {
+    const reports = await this.adminService.getAllReports(page, limit);
+    return new ApiResponse(200, 'Reports retrieved successfully', reports);
   }
 }
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -3,16 +3,17 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../users/common/schemas/user.schema';
 import { Model } from 'mongoose';
 import { ApiResponse } from '../users/dto/response.dto';
+import { ReportUser, ReportUserDocument } from '../report/schemas/report-user.schema';
 
 @Injectable()
 export class AdminService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    @InjectModel(ReportUser.name)
+    private readonly reportUserModel: Model<ReportUserDocument>,
   ) {}
 
-  async userBanManagment(
-    userId: string,
-  ): Promise<ApiResponse<any>> {
+  async userBanManagment(userId: string): Promise<ApiResponse<any>> {
     const user = await this.userModel.findById(userId);
     if (!user) {
       throw new NotFoundException('User not found');
@@ -35,5 +36,62 @@ export class AdminService {
   async deleteAllUsersExceptAdmins(): Promise<void> {
     // Elimina todos los usuarios excepto aquellos con el rol de 'admin'
     await this.userModel.deleteMany({ role: { $ne: 'admin' } });
+  }
+
+  async getAllUsers(page: number = 1, limit: number = 10): Promise<any> {
+    const skip = (page - 1) * limit;
+
+    const [users, total] = await Promise.all([
+      this.userModel
+        .find()
+        .select(
+          '_id name lastName profilePicture email phone role municipality neighborhood isBanned personalDescription coverageArea address preferences skills',
+        ) // Selecciona solo los campos necesarios
+        .populate([
+          { path: 'preferences', select: 'skillName -_id' }, // Populate para preferencias
+          { path: 'skills', select: 'skillName -_id' }, // Populate para habilidades
+        ])
+        .skip(skip)
+        .limit(limit)
+        .exec(),
+      this.userModel.countDocuments().exec(),
+    ]);
+
+    return {
+      users,
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    };
+  }
+
+  async getAllReports(page: number = 1, limit: number = 10): Promise<any> {
+    const skip = (page - 1) * limit;
+
+    const [reports, total] = await Promise.all([
+      this.reportUserModel
+        .find()
+        .populate({
+          path: 'reporterUserId',
+          select: 'name lastName email phone', // Datos del que reporta
+        })
+        .populate({
+          path: 'reportedUserId',
+          select: 'name lastName email phone', // Datos del reportado
+        })
+        .skip(skip)
+        .limit(limit)
+        .exec(),
+      this.reportUserModel.countDocuments().exec(),
+    ]);
+
+    return {
+      reports,
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    };
   }
 }
